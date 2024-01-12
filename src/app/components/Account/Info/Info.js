5c6f8ecdb4a4cab5a222c281d4e28c1ebc71e64e
grGrimales <grojasgrimales@gmail.com>
2024-01-12 14:26:20 -0500

    se modifica redireccion para account

--- src/app/components/Account/Info/Info.js
+++ src/app/components/Account/Info/Info.js
@@ -8,14 +8,16 @@ import { DateTime } from "luxon";
 
 export function Info(){
     const {user} = useAuth();
+
+    console.log(user)
     return(
         <div className={styles.info}>
         <Button icon className={styles.user}>
             <Icon name="user outline"/>
         </Button>
-        <h3 className={styles.username}>{user.username}</h3>
-        <h4 className={styles.email}>{user.email}</h4>
-        <p className={styles.createdAt}>Member since: {DateTime.fromISO(user.createdAt, {locale: "en"}).toFormat("DDD")}</p>
+        <h3 className={styles.username}>{user?.username}</h3>
+        <h4 className={styles.email}>{user?.email}</h4>
+        <p className={styles.createdAt}>Member since: {DateTime.fromISO(user?.createdAt, {locale: "en"}).toFormat("DDD")}</p>
         </div>
     )
 } 
