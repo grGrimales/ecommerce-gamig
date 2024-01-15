756f153e813bf61865644a4f3fdc7c3f3a3e8fa2
grGrimales <grojasgrimales@gmail.com>
2024-01-15 05:28:00 -0500

    se agrega condicional para cart

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -4,76 +4,70 @@ import { Cart } from "@/api/cart";
 import { set } from "lodash";
 import { createContext, useEffect, useState } from "react";
 
-
-
 export const CartContext = createContext();
 
-
 const cartCtrl = new Cart();
-export function CartProvider (props) {
-    const  {children} = props;
-    const [currentStep, setCurrentStep] = useState(1);
+export function CartProvider(props) {
+  const { children } = props;
+  const [currentStep, setCurrentStep] = useState(1);
 
+  const [cart, setCart] = useState(null);
+  const [total, setTotal] = useState(cartCtrl.count());
 
-
-    const  [cart, setCart] = useState(null);
-    const [total, setTotal] = useState(cartCtrl.count());
-
-    useEffect(() => {
+  useEffect(() => {
     const response = cartCtrl.getAll();
     setCart(response);
-    }, []);
+  }, []);
 
-    const nextStep = () => {
-        setCurrentStep(currentStep + 1);
-      };
-      
-      const previousStep = () => {
-        setCurrentStep(currentStep - 1);
-      };
-
-    const addCart = (gameId) => {
-        cartCtrl.add(gameId);
-        refreshCart();
-
+  const nextStep = () => {
+    setCurrentStep(currentStep + 1);
+    if (currentStep === 3) {
+      setTimeout(() => {
+        setCurrentStep(1);
+      }, 3000);
     }
+  };
 
-    const refreshCart = () => {
-        setTotal(cartCtrl.count());
-        setCart(cartCtrl.getAll());
-    }
+  const previousStep = () => {
+    setCurrentStep(currentStep - 1);
+  };
 
-    const changeQuantityItem = (gameId, quantity) => {
-        cartCtrl.changeQuantity(gameId, quantity);
-        refreshCart();
-    }
+  const addCart = (gameId) => {
+    cartCtrl.add(gameId);
+    refreshCart();
+  };
 
-    const deleteItem = (gameId) => {
-        cartCtrl.deleteItem(gameId);
-        refreshCart();
-    }
+  const refreshCart = () => {
+    setTotal(cartCtrl.count());
+    setCart(cartCtrl.getAll());
+  };
 
-    const deleteAllItems = () => {
-        cartCtrl.deleteAllItems();
-        refreshCart();
-    }
-    
-    const data = {
-        cart,
-        total,
-        currentStep,
-        nextStep,
-        previousStep,
-        addCart,
-        deleteItem,
-        deleteAllItems,
-        changeQuantityItem,
-    };
+  const changeQuantityItem = (gameId, quantity) => {
+    cartCtrl.changeQuantity(gameId, quantity);
+    refreshCart();
+  };
 
+  const deleteItem = (gameId) => {
+    cartCtrl.deleteItem(gameId);
+    refreshCart();
+  };
 
-    return (
-        <CartContext.Provider value={data}>
-            {children}
-        </CartContext.Provider>
-    )
+  const deleteAllItems = () => {
+    cartCtrl.deleteAllItems();
+    refreshCart();
+  };
+
+  const data = {
+    cart,
+    total,
+    currentStep,
+    nextStep,
+    previousStep,
+    addCart,
+    deleteItem,
+    deleteAllItems,
+    changeQuantityItem,
+  };
+
+  return <CartContext.Provider value={data}>{children}</CartContext.Provider>;
 }
